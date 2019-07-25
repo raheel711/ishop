c624d043e5ac2cc957c072808362c84a6faf5057
Raheel Aftab <raheeel711@gmail.com>
2019-07-25 11:54:18 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ishop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
